fae5f3e83a357b7baa2975a7a3e1ae9fcfb42e74
PaulStudy <95910444+PaulStudy@users.noreply.github.com>
2023-09-12 16:47:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ let today = new Date();
 let num_month = today . getMonth() +1 ; 
 let num_today = today . getDate()
 let numdaysInMonth = daysInMonth(num_month , year)
-let ostatok = numdaysInMonth-num_today+1
-let ostatokTomorrow = ostatok - 1
+let ostatok = numdaysInMonth-num_today
+let ostatokToday = ostatok + 1
 
 let summaPerDay = Math.floor(9550 / numdaysInMonth)
-let summaToday = summaPerDay * ostatok + 450
-let summaTomorrow = summaPerDay * ostatokTomorrow + 450
+let summaToday = summaPerDay * ostatokToday + 450
+let summaTomorrow = summaPerDay * ostatok + 450
 
 function pogovorkaFM() {
      let selector = document.getElementById(`joke`)
      let selector2 = document.getElementById(`author`)
      selector.innerText = 'С сегодняшнего дня счет = ' + summaToday
-     selector2.innerText = ' 9550 делим на ' + numdaysInMonth + ', умножаем на оставшиеся ' + ostatok + ' дней и прибавляем 450'
+     selector2.innerText = ' 9550 делим на ' + numdaysInMonth + ', умножаем на оставшиеся ' + ostatokToday + ' дней и прибавляем 450'
 
 }
 
@@ -33,7 +33,7 @@ function pogovorkaFM2() {
     let selector = document.getElementById(`joke`)
     let selector2 = document.getElementById(`author`)
     selector.innerText = 'С завтрашнего дня счет = ' + summaTomorrow
-    selector2.innerText = ' 9550 делим на ' + numdaysInMonth + ', умножаем на оставшиеся с завтрашнего дня ' + ostatokTomorrow + ' дней и прибавляем 450'
+    selector2.innerText = ' 9550 делим на ' + numdaysInMonth + ', умножаем на оставшиеся с завтрашнего дня ' + ostatok + ' дней и прибавляем 450'
 }
 
 
