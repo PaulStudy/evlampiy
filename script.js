18a193325fd682094860fc86fd59b0658a5c21ae
PaulStudy <95910444+PaulStudy@users.noreply.github.com>
2023-09-12 17:01:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ let numdaysInMonth = daysInMonth(num_month , year)
 let ostatok = numdaysInMonth-num_today
 let ostatokToday = ostatok + 1
 
-let summaPerDay = Math.floor(9550 / numdaysInMonth)
+let summaPerDay = 9550 / numdaysInMonth
 let summaToday = summaPerDay * ostatokToday + 450
 let summaTomorrow = summaPerDay * ostatok + 450
 
 function pogovorkaFM() {
      let selector = document.getElementById(`joke`)
      let selector2 = document.getElementById(`author`)
-     selector.innerText = 'С сегодняшнего дня счет = ' + summaToday
+     selector.innerText = 'С сегодняшнего дня счет = ' + Math.ceil(summaToday)
      selector2.innerText = ' 9550 делим на ' + numdaysInMonth + ', умножаем на оставшиеся ' + ostatokToday + ' дней и прибавляем 450'
 
 }
@@ -32,8 +32,6 @@ function pogovorkaFM() {
 function pogovorkaFM2() {
     let selector = document.getElementById(`joke`)
     let selector2 = document.getElementById(`author`)
-    selector.innerText = 'С завтрашнего дня счет = ' + summaTomorrow
+    selector.innerText = 'С завтрашнего дня счет = ' + Math.ceil(summaTomorrow)
     selector2.innerText = ' 9550 делим на ' + numdaysInMonth + ', умножаем на оставшиеся с завтрашнего дня ' + ostatok + ' дней и прибавляем 450'
 }
-
-
